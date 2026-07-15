77831d734d82f8d5d266a2e7d00477e82633b4fd
vilmarmorgan <3477701+vilmarmorgan@users.noreply.github.com>
2026-07-14 18:19:57 -0700

    Play Pupcup menu link opens in a new tab

--- nav.js
+++ nav.js
@@ -18,7 +18,7 @@
     { label: 'Buy Now', soon: true },
     { label: 'How to Play', soon: true },
     { label: 'Kickstarter Backers', href: '/kickstarter/' },
-    { label: 'Play Pupcup', href: 'https://playpupcup.com/', external: true, sub: 'The Family Friendly Game' }
+    { label: 'Play Pupcup', href: 'https://playpupcup.com/', external: true, newTab: true, sub: 'The Family Friendly Game' }
   ];
 
   var html = LINKS.map(function (l) {
@@ -26,12 +26,13 @@
       return '<div class="nav-menu-soon"><span class="nav-menu-soon-label">' + l.label +
              '</span><span class="nav-menu-soon-sub">coming soon</span></div>';
     }
-    var rel = l.external ? ' rel="noopener"' : '';
+    var attrs = l.external ? ' rel="noopener"' : '';
+    if (l.newTab) attrs += ' target="_blank"';
     if (l.sub) {
-      return '<div class="nav-menu-pup"><a href="' + l.href + '"' + rel + '>' + l.label + '</a>' +
+      return '<div class="nav-menu-pup"><a href="' + l.href + '"' + attrs + '>' + l.label + '</a>' +
              '<span class="nav-menu-pup-sub">' + l.sub + '</span></div>';
     }
-    return '<a href="' + l.href + '"' + rel + '>' + l.label + '</a>';
+    return '<a href="' + l.href + '"' + attrs + '>' + l.label + '</a>';
   }).join('\n    ');
 
   var nav = document.querySelector('.nav-menu-links');
